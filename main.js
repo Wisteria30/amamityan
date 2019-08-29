98ca8c9ec5e88bbf3db0fdc99c349b1d4d701795
wisteria30 <wis30@wisteria30.local>
2019-08-30 02:47:35 +0900

    change wav to mp3

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const audioElem = {
-    n: ["<p>ありがとう</p>" + "<p>ございます</p>", "./audio/arigatou.wav"],
-    e: ["え？", "./audio/e.wav"],
-    d: ["えぇ!?", "./audio/ee.wav"],
-    j: ["えへへ", "./audio/ehehe.wav"],
-    h: ["はぁ...", "./audio/haa.wav"],
-    y: ["はい!", "./audio/hai.wav"],
-    k: ["わぁ!!!", "./audio/kokeru.wav"],
-    p: ["Pさん♪", "./audio/Psan.wav"]
+    n: ["<p>ありがとう</p>" + "<p>ございます</p>", "./audio/arigatou.mp3"],
+    e: ["え？", "./audio/e.mp3"],
+    d: ["えぇ!?", "./audio/ee.mp3"],
+    j: ["えへへ", "./audio/ehehe.mp3"],
+    h: ["はぁ...", "./audio/haa.mp3"],
+    y: ["はい!", "./audio/hai.mp3"],
+    k: ["わぁ!!!", "./audio/kokeru.mp3"],
+    p: ["Pさん♪", "./audio/Psan.mp3"]
 };
 
 window.onload = function () {
